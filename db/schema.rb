fe2056c25aca8fe0546cad3eb9e670b7cfa34b12
Doga Tuncay <dogatuncay@hotmail.com>
2015-08-01 16:53:43 +0530

    added is blocked to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801074010) do
+ActiveRecord::Schema.define(version: 20150801112051) do
 
   create_table "average_caches", force: true do |t|
     t.integer  "rater_id"
@@ -100,12 +100,12 @@ ActiveRecord::Schema.define(version: 20150801074010) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20150801074010) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "is_blocked",             default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
