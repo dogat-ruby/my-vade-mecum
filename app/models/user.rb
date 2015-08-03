53ae3aaf88a8a782a7ae7c469c20365ae822cd9c
Dogat <dogatuncay@hotmail.com>
2015-08-03 06:56:14 +0530

    disabled user confirmation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-  :trackable, :validatable, :confirmable
+  :trackable, :validatable
+  # , :confirmable
   # associations
   has_many :books
   has_many :reviews, dependent: :destroy
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #host
-  config.action_mailer.default_url_options = { :host => 'tlibrary.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'tlibrary.herokuapp.com' }
 end
