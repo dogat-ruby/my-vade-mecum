34ecf936e0a922965f18cdd1dc4d06e0fa7b6f8f
Dogat <dogatuncay@hotmail.com>
2015-08-03 06:47:25 +0530

    added mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #host
+  config.action_mailer.default_url_options = { :host => 'tlibrary.herokuapp.com/' }
 end
